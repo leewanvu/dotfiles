65f1e91ff396c495c4a8ddcccaac63ed5ed0d616
FA-VuLQ <vulq@fireapps.vn>
2020-12-30 18:15:33 +0700

    Init

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-read -p "install configs ? y/n: " is_install
-
-if [ "$is_install" == "n" ]; then
-    echo "See you again!"
-    exit 1;
-fi
+# read -p "install configs ? y/n: " is_install
+#
+# if [ "$is_install" == "n" ]; then
+#     echo "See you again!"
+#     exit 1;
+# fi
 
 print_bold() {
     printf "\x1b[1m$1\x1b[0m\n"
 }
 
-print_bold "$PWD/neovim";
+# print_bold "$PWD/neovim";
+# print_bold "$HOME/.configs";
+
+for file in *; do
+    linkto="$HOME/.config/$file"
+    linkfrom="$PWD/$file"
+
+    echo "Link from: $linkfrom to $linkto"
+done
