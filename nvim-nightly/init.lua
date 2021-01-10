e8128214e4316711fa9f9ed6d4544ec543cd30c2
Vu Le <leewanvu@gmail.com>
2021-01-10 23:03:29 +0700

    Remove whitespace on save

--- nvim-nightly/init.lua
+++ nvim-nightly/init.lua
@@ -4,3 +4,7 @@ vim.g.mapleader = ' '
 require('v-plugins')
 require('v-options')
 require('v-mappings')
+
+-- Auto commands
+-- Remove trailing whitespace on save
+vim.cmd "autocmd BufWritePre * %s/\\s\\+$//e"
--- nvim-nightly/lua/v-plugins.lua
+++ nvim-nightly/lua/v-plugins.lua
@@ -91,7 +91,7 @@ return require('packer').startup(function()
     config = function()
       vim.g.gitblame_enabled = 0
       vim.g.gitblame_message_template = ' <author> • <date> • <summary>'
-      vim.g.gitblame_date_format = '%c' 
+      vim.g.gitblame_date_format = '%c'
     end
   }
   -- use {
