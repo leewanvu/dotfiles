fefbd0f86e11392a62b104a8eee2728d41fde89c
FA-VuLQ <vulq@fireapps.vn>
2021-01-25 09:30:30 +0700

    Clean

--- nvim-nightly/init.lua
+++ nvim-nightly/init.lua
@@ -4,7 +4,3 @@ vim.g.mapleader = ' '
 require('v-plugins')
 require('v-options')
 require('v-mappings')
-
--- Auto commands
--- Remove trailing whitespace on save
--- vim.cmd "autocmd BufWritePre * %s/\\s\\+$//e"
