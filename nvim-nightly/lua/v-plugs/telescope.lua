442c4499707031fb50ce13401c82aaae3963add0
FA-VuLQ <vulq@fireapps.vn>
2021-03-17 17:47:11 +0700

    Update telescope

--- nvim-nightly/lua/v-plugs/telescope.lua
+++ nvim-nightly/lua/v-plugs/telescope.lua
@@ -6,7 +6,7 @@ function M.setup()
 
   local opts = { noremap = true, silent = true }
 
-  vim.api.nvim_set_keymap('n', '<leader>p', "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
+  vim.api.nvim_set_keymap('n', '<leader>p', "<cmd>lua require('telescope.builtin').find_files({ find_command = { 'rg', '--files', '--hidden', '--no-ignore-vcs', '-g', '!{node_modules,.git,vendor}' } })<cr>", opts)
   vim.api.nvim_set_keymap('n', '<leader>g', "<cmd>lua require('telescope.builtin').git_status()<cr>", opts)
   -- vim.api.nvim_set_keymap('n', '<leader>/', "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
   vim.api.nvim_set_keymap('n', '<leader>/', "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>", opts)
