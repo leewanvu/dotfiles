3660b36e3d13ce470faf7b3d23d159748352e34e
FA-VuLQ <vulq@fireapps.vn>
2021-03-23 13:29:25 +0700

    refactor: options

--- nvim-nightly/lua/vux/options.lua
+++ nvim-nightly/lua/vux/options.lua
@@ -16,11 +16,12 @@ opt.showtabline    = 2
 opt.hidden = true
 opt.number = true
 opt.relativenumber = true
-opt.mouse = 'n' -- 'a'
+opt.mouse = 'a'
 opt.scroll = 10
 opt.lazyredraw = true
 opt.visualbell = true
 opt.fileencoding = 'utf-8'
+opt.encoding = 'utf-8'
 opt.showmode = false
 opt.showcmd = true
 opt.cmdheight = 1
@@ -34,34 +35,23 @@ opt.ignorecase = true
 opt.smartcase = true
 opt.updatetime = 100
 opt.iskeyword = opt.iskeyword + '$'
--- opt.backup = 'on'
+opt.backup = false
 opt.signcolumn = 'yes'
 opt.termguicolors = true
 opt.syntax = 'on'
+-- opt.listchars = "eol:↲,tab:»\\ "
+-- opt.list = true
+opt.clipboard = 'unnamedplus'
+opt.history = 1000
+opt.completeopt = 'menuone,noinsert,noselect'
+opt.undolevels = 1000
+opt.wildmenu = true
+opt.wrap = false
+opt.errorbells = false
+opt.writebackup = false
+opt.swapfile = false
 -- opt.shortmess = opt.shortmess + 'c'
--- opt.clipboard = opt.clipboard + 'unnamedplus'
-
--- opt('o', 'termguicolors', true)
--- opt('b', 'syntax', 'on')
--- opt('o', 'listchars', "eol:↲,tab:»\\ ")
--- opt('o', 'list', true)
-
--- set clipboard+=unnamedplus
--- set shortmess+=c
--- set iskeyword+=$
-vim.api.nvim_exec([[
-  set noswapfile
-  set nowritebackup
-  set nobackup
-  set noerrorbells
-  set nowrap
-  set completeopt=menuone,noinsert,noselect
-  set wildmenu
-  set noshowmode
-  set encoding=utf-8
-  set history=1000
-  set undolevels=1000
-  ]], '')
+vim.o.shortmess = vim.o.shortmess .. 'c'
 
 -- set foldmethod=expr
 -- set foldexpr=nvim_treesitter#foldexpr()
