768195b4f1fc0000733bbe86df9a43f35a07ce9a
FA-VuLQ <vulq@fireapps.vn>
2021-03-25 13:29:31 +0700

    change setting barbar, lualine

--- nvim-nightly/lua/vux/lualine.lua
+++ nvim-nightly/lua/vux/lualine.lua
@@ -11,7 +11,8 @@ require('lualine').setup{
     lualine_a = {'mode'},
     lualine_b = {'branch'},
     lualine_c = {'filename', 'diff'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    -- lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
