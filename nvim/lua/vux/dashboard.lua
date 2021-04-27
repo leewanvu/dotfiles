7963b87f77f983bbf2dd0471e4081baa94835046
FA-VuLQ <vulq@fireapps.vn>
2021-04-27 11:14:20 +0700

    Updated dashboard telescope find_files

--- nvim/lua/vux/dashboard.lua
+++ nvim/lua/vux/dashboard.lua
@@ -14,7 +14,7 @@ vim.g.dashboard_custom_header = {
    '                     ▀█▀                   ',
 }
 vim.g.dashboard_custom_section = {
-    a = {description = {'  Find File          '}, command = 'Telescope find_files'},
+    a = {description = {'  Find File          '}, command = 'Telescope find_files find_command=rg,--ignore,--hidden,--files,--no-ignore-vcs,-g,!node_modules,-g,!.git,-g,!vendor'},
     b = {description = {'  Recently Used Files'}, command = 'Telescope oldfiles'},
     c = {description = {'  Load Last Session  '}, command = 'SessionLoad'},
     d = {description = {'  Find Word          '}, command = 'Telescope live_grep'},
