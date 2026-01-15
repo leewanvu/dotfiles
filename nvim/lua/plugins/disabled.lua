07c68face7613d190296dd6de178ec7fe7916c44
Vu Le Quang <vulq@firegroup.io>
2026-01-15 15:08:08 +0700

    refactor: disable unused neovim plugins for better performance
    - Disable incline.nvim plugin to reduce startup overhead
    - Remove markdown and vscode LazyVim extras
    - Pin gitsigns to specific commit for stability
    - Update lazy lock file to reflect plugin changes

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,4 +3,6 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   -- { "echasnovski/mini.indentscope", enabled = false },
   -- { "folke/flash.nvim", enabled = false },
+  -- { "lewis6991/gitsigns.nvim", enabled = false },
+  { "lewis6991/gitsigns.nvim", commit = "7010000889bfb6c26065e0b0f7f1e6aa9163edd9" },
 }
