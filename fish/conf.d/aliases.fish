71c1de6a324f64a109493262bcc96791721e04c1
Vu Le <leewanvu@gmail.com>
2026-02-23 10:19:50 +0700

    clean alias fish

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -23,15 +23,12 @@ alias gd='git diff'
 alias gl='git log --oneline'
 
 function current_branch
-  set ref (git symbolic-ref HEAD 2> /dev/null); or \
-  set ref (git rev-parse --short HEAD 2> /dev/null); or return
-  echo $ref | sed -e 's|^refs/heads/||'
+    set ref (git symbolic-ref HEAD 2> /dev/null); or set ref (git rev-parse --short HEAD 2> /dev/null); or return
+    echo $ref | sed -e 's|^refs/heads/||'
 end
 
 alias gpl="git pull origin (current_branch)"
 alias gp="git push origin (current_branch)"
-
-alias ar="make -f ~/Work/ar/docker/bin/Makefile"
 
 alias blt="blueutil -p"
 
